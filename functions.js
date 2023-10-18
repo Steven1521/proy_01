53eb30a73ffaa9c63e00ed22641b5faf959c787d
steven1521 <stevensanchez1521@gmail.com>
2023-10-17 21:10:24 -0500

    proyecto 1 node js

---
+++ functions.js
@@ -0,0 +1,25 @@
+//define la función sumar(add)
+function add(a,b) {
+    return a + b;
+}
+
+//define la función restar(substract)
+function substract(a,b) {
+    return a - b;
+}
+
+//define la función multiplicar(multiply)
+function multiply(a,b) {
+    return a * b;
+}
+
+//define la funcion dividir(divide)
+function divide(a,b) { 
+    return a / b;
+}
+
+
+exports.add = add;
+exports.substract = substract;
+exports.multiply = multiply;
+exports.divide = divide;
